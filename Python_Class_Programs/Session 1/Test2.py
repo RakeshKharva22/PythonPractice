2c7988f16ec4aac271ea6570a891dd23656b792f
RakeshKharva <rakeshkharva.tops@gmail.com>
2022-09-09 13:22:49 +0530

    09-09-22 practice

---
+++ Python_Class_Programs/Session 1/Test2.py
@@ -0,0 +1,27 @@
+"""
+a = input("Enter A :")
+print("A : ",a)
+print(type(a))
+
+
+a = int(input("Enter A : "))
+print("A : ",a)
+print(type(a))
+
+
+
+a = float(input("Enter A : "))
+print("A : ",a)
+print(type(a))
+
+a = int(input("Enter A : "))
+b = int(input("Enter B : "))
+print("A : ",a,",B : ",b)
+
+"""
+
+a = int(input("Enter A : "))
+b = int(input("Enter B : "))
+print("Before Swapping A = ",a,",B = ",b)
+a,b = b,a
+print("After Swapping A = ",a,",B = ",b)
