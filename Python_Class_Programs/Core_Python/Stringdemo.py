8787964a8d8819a68b59a3a379eb8b66a58ba018
RakeshKharva <rakeshkharva.tops@gmail.com>
2022-09-12 16:13:48 +0530

    12-9-22

---
+++ Python_Class_Programs/Core_Python/Stringdemo.py
@@ -0,0 +1,27 @@
+s= "Tops Technologies"
+
+#for i in s:
+ #   print(i)
+
+#String Functions.
+ 
+print(s.capitalize())
+print(s.casefold())
+print(s.center(50,"*"))
+print(s.endswith("es"))
+print(s.find("log"))
+print(s.index("T",3))
+print("tops123".isalnum())
+print("tops".isalnum())
+print("123".isalnum())
+print("123".isalpha())
+print("123".isdigit())
+print("123".isnumeric())
+print("tops".isalpha())
+print(s.lower())
+print(s.upper())
+print(s.istitle())
+print("hello".replace("l","w"))
+print(s.startswith("To"))
+print(s.swapcase())
+print("python programming language".title())
